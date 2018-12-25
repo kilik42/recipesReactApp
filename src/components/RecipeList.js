14e590a762f6c0162aa903f55267062499ca63dd
marvin evins <maevins@cps.edu>
2018-12-24 22:13:32 -0600

    recipe ui

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -6,13 +6,23 @@ import RecipeSearch from './RecipeSearch';
  * RecipeList
  */
 export class RecipeList extends Component { // eslint-disable-line react/prefer-stateless-function
+
+
   render() {
+    const{recipes} = this.props;
     return (
       <React.Fragment>
 
         <RecipeSearch/>
-
-        
+<div className= "container my-5">
+  {/* title */}
+<div className="row">
+  <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+     <h1 className="text-slanted">recipe list</h1>
+  </div>
+</div>
+</div>
+{/* end of title */}
         <Recipe/>
       </React.Fragment>
     );
